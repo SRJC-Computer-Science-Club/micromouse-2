dd636cb801053008fd00e6bc78d0ba129efd2619
Josh Alan <thedatasmith1@gmail.com>
2018-02-09 13:56:56 -0800

    Added a comment

--- MicroMouse2/MicroMouse2/test_functions.h
+++ MicroMouse2/MicroMouse2/test_functions.h
@@ -3,13 +3,15 @@
 
 using namespace micromouse;
 
+/*
+* Test function for the coord class.
+*/
 coord test_coord()
 {
 	coord c0 = coord(0, 0);
 	coord c1 = coord(3, 7);
-	coord c2 = coord(1, 2);
+	coord c2 = coord(1, 5);
 	coord c3 = c0 + c1 + c2;
-	coord c4 = 5 * c2 - c1;
 
 	return c3;
 }
